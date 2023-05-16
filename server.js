6e1d6cc8f3675b96ca3621fbbe9db730923c1c5d
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2023-05-16 15:43:13 +0200

    server.js wrap up

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 const http = require('node:http');
 
-const host = '127.0.0.1';
-const port = 8080;
+const HOST = '127.0.0.1';
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   // --- several ways to handle the origin ---
+
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5050');
-  // OR
+
+  // * * * OR * * *
+
   // res.setHeader('Access-Control-Allow-Origin', '*');
-  // OR
-  const allowedOrigins = [
-    'http://localhost:5050',
-    'http://localhost:8020',
-    'http://example.com',
-    'https://example.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+
+  // * * * OR * * *
+
+  // const allowedOrigins = [
+  //   'http://localhost:5050',
+  //   'http://localhost:8020',
+  //   'http://example.com',
+  //   'https://example.com'];
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //   res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+
   // ------
 
   res.end('Hello from the server!');
 });
 
-server.listen(port, host, () => {
-  console.log(`🚀 Server is running @ http://${host}:${port}`);
+server.listen(PORT, HOST, () => {
+  console.log(`🚀 Server is running @ http://${HOST}:${PORT}`);
 });
